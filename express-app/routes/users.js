9a33c796156fbeabc51e918d7bd0c792b3c0cebe
anton_davidovich <dvd14@ukr.net>
2019-02-19 00:27:44 +0200

    Style user.js + add  new variables

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -3,8 +3,8 @@ const router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  res.render('users', { users: 'Users page!', enter:'Enter your name' });
   res.send('respond with a resource');
-  
 });
 
 module.exports = router;
